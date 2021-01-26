c9ae1690322bf5b8fdc3123f759368b7d4236402
Enxhi Salaj <enxhi_salaj@yahoo.com>
2021-01-26 11:57:36 -0500

    added files to cache and fixed minor issues

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,14 +1,20 @@
+// Files that need to be cached
+
 const FILES_TO_CACHE = [
     "/",
     "/index.html",
-    "index.js",
-    "manifest.webmanifest"
+    "/index.js",
+    "/indexedDB.js",
+    "/manifest.webmanifest",
+    "/styles.css",
+    "/icons/icon-192x192.png",
+    "/icons/icon-512x512.png"
 ];
 
 const CACHE_NAME = "static-cache-v2";
 const DATA_CACHE_NAME = "data-cache-v1";
 
-// install
+// install and register the service worker
 self.addEventListener("install", function (evt) {
     evt.waitUntil(
         caches.open(CACHE_NAME).then(cache => {
@@ -16,10 +22,11 @@ self.addEventListener("install", function (evt) {
             return cache.addAll(FILES_TO_CACHE);
         })
     );
-
+    // tells the browser to activate Service-worker right after installing
     self.skipWaiting();
 });
 
+// activates clear out old cache
 self.addEventListener("activate", function (evt) {
     evt.waitUntil(
         caches.keys().then(keyList => {
@@ -37,7 +44,7 @@ self.addEventListener("activate", function (evt) {
     self.clients.claim();
 });
 
-// fetch
+// fetch data from API
 self.addEventListener("fetch", function (evt) {
     // cache successful requests to the API
     if (evt.request.url.includes("/api/")) {
@@ -45,7 +52,7 @@ self.addEventListener("fetch", function (evt) {
             caches.open(DATA_CACHE_NAME).then(cache => {
                 return fetch(evt.request)
                     .then(response => {
-                        // If the response was good, clone it and store it in the cache.
+                        // In case of successful response, clone it and store it in the cache.
                         if (response.status === 200) {
                             cache.put(evt.request.url, response.clone());
                         }
@@ -53,7 +60,7 @@ self.addEventListener("fetch", function (evt) {
                         return response;
                     })
                     .catch(err => {
-                        // Network request failed, try to get it from the cache.
+                        // In case of Network request failed, try to get it from the cache.
                         return cache.match(evt.request);
                     });
             }).catch(err => console.log(err))
@@ -62,8 +69,7 @@ self.addEventListener("fetch", function (evt) {
         return;
     }
 
-    // if the request is not for the API, serve static assets using "offline-first" approach.
-    // see https://developers.google.com/web/fundamentals/instant-and-offline/offline-cookbook#cache-falling-back-to-network
+    // if the request is not for the API, it serves static assets using "offline-first"
     evt.respondWith(
         caches.match(evt.request).then(function (response) {
             return response || fetch(evt.request);
